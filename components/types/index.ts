eb2473258ea814e7c81efe03891f464dc4cfa4d4
kimute <jkdnekki@icloud.com>
2022-09-19 03:01:57 +0900

    modify json

--- components/types/index.ts
+++ components/types/index.ts
@@ -1,11 +1,8 @@
 export interface IMylocationData {
   id: number;
   title: string;
-  name: string;
   category: string;
   imgSrc: string;
-  latitude: number;
-  longitude: number;
   location: number[];
   details: string;
 }
